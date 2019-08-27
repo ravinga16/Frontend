504b8ed2d850471f48b0c0acb0f1f38e5dccb647
Ravinga Perera <ravinga_p@3logixs.lk>
2019-08-27 18:07:27 +0530

    user folder update

--- src/Component/UserProfile.js
+++ src/Component/UserProfile.js
@@ -25,24 +25,16 @@ render() {
         <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
           <MDBNavbarNav left>
             <MDBNavItem active>
-              <MDBNavLink to="#!">Home</MDBNavLink>
+              <MDBNavLink to="/user/home/">Home</MDBNavLink>
             </MDBNavItem>
             <MDBNavItem>
-              <MDBNavLink to="#!">Search</MDBNavLink>
+              <MDBNavLink to="/user/Search/">Search</MDBNavLink>
             </MDBNavItem>
             <MDBNavItem>
-              <MDBNavLink to="#!">Order</MDBNavLink>
+              <MDBNavLink to="/user/Order/">Order</MDBNavLink>
             </MDBNavItem>            
           </MDBNavbarNav>
-          <MDBNavbarNav right>
-            <MDBNavItem>
-              <MDBFormInline waves>
-                <div className="md-form my-0">
-                  <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-                </div>
-              </MDBFormInline>
-            </MDBNavItem>
-          </MDBNavbarNav>
+         
         </MDBCollapse>
       </MDBNavbar>
     </Router>
