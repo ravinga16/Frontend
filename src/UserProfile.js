f2e67b263824500af9c9d597a2864084ffaccf03
ravinga <37566894+ravinga16@users.noreply.github.com>
2019-08-30 23:20:34 +0530

    Add files via upload

---
+++ src/UserProfile.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import {  MDBRow, MDBCol } from "mdbreact";
+
+import UserNavBar from "./UserNavBar";
+import Userprofilecard from "./Userprofilecard";
+
+export default class UserProfile extends Component {
+state = {
+  isOpen: false
+};
+
+toggleCollapse = () => {
+  this.setState({ isOpen: !this.state.isOpen });
+}
+
+render() {
+  return (
+    <div>
+      <UserNavBar/>
+      <MDBRow>
+        <MDBCol md="6"><Userprofilecard/></MDBCol>
+        <MDBCol md="6">.col-md-4</MDBCol>
+        {/* <MDBCol md="4">.col-md-4</MDBCol> */}
+      </MDBRow>
+    </div>
+    
+    );
+  }
+}
+
+// ReactDOM.render(<UserProfile />, document.getElementById('root'));
---
+++ src/Userprofilecard.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
+
+class Userprofilecard extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            UserName:'',
+            Email:'',
+            Password:'',
+            ContactNumber:''
+        }
+        this.handleCancel=this.handleCancel.bind(this);
+        this.handleEditProfile=this.handleEditProfile.bind(this);
+        this.handleUpdate=this.handleUpdate.bind(this);
+    }
+
+ 
+    handleEditProfile(event){
+        event.preventDefault();
+        document.getElementById("showProfile").style.display = "none";
+        document.getElementById("editProfile").style.display="block";
+    }
+    handleUpdate(event){
+        event.preventDefault();
+        // this.setState({UserName:document.getElementById("UserName").value});
+        // this.setState({Email:document.getElementById("Email").value});
+        // this.setState({Password:document.getElementById("Password").value});
+        // this.setState({ContactNumber:document.getElementById("ContactNumber").value});
+        this.state.UserName=document.getElementById("UserName").value;
+        this.state.Email=document.getElementById("Email").value;
+        this.state.Password=document.getElementById("Password").value;
+        this.state.ContactNumber=document.getElementById("ContactNumber").value;
+        console.log(this.state);
+    }
+    handleCancel(event){
+        event.preventDefault();
+        document.getElementById("editProfile").style.display="none";
+        document.getElementById("showProfile").style.display="block";
+    }
+    render() {
+        return (
+            <MDBCol>
+                <MDBCard style={{ width: "30rem" }}>
+                    <MDBCardImage className="img-fluid" src="https://image.shutterstock.com/image-vector/user-icon-trendy-flat-style-260nw-418179865.jpg" waves />
+                    <MDBCardBody>
+                   
+                        <form style={{ textAlign: "left",display:"block" ,fontSize:"13px"}} id="showProfile">
+                        
+                            <div class="row">
+                                <div class="col-md-5">User Name:
+                        </div>
+                                <div class="col-md-5">
+                                    <input type="text" name="UserName" placeholder="Ravinga Sewwandi" disabled></input>
+                                </div>
+                            </div><br></br>
+                            <div class="row">
+                                <div class="col-md-5">Email:
+                        </div>
+                                <div class="col-md-5">
+                                    <input type="email" name="Email" placeholder="ravinga.p@eyepax.com" disabled></input>
+                                </div>
+                            </div><br></br>
+                            <div class="row">
+                                <div class="col-md-5">Password:
+                        </div>
+                                <div class="col-md-5">
+                                    <input type="password" name="Password" placeholder="12345" disabled></input>
+                                </div>
+                            </div><br></br>
+                            <div class="row">
+                                <div class="col-md-5">Contact Number:
+                        </div>
+                                <div class="col-md-5">
+                                    <input type="text" name="ContactNumber" placeholder="071404040" disabled></input>
+                                </div>
+                            </div><br></br>
+                            <div class="row">
+                            <div class="col-md-5"> <MDBBtn onClick={this.handleEditProfile}>Edit Profile</MDBBtn></div>
+                            </div>
+
+                  
+                        </form>
+
+                    
+                        
+                        <form onSubmit={this.handleUpdate} style={{ textAlign: "left",display:"none" ,fontSize:"13px"}} id="editProfile"> 
+                      
+                            <div class="row">
+                                <div class="col-md-5">User Name:
+                        </div>
+                                <div class="col-md-5">
+                                    <input type="text" id="UserName" placeholder="Ravinga Sewwandi" ></input>
+                                </div>
+                            </div><br></br>
+                            <div class="row">
+                                <div class="col-md-5">Email:
+                        </div>
+                                <div class="col-md-5">
+                                    <input type="email" id="Email" placeholder="ravinga.p@eyepax.com" ></input>
+                                </div>
+                            </div><br></br>
+                            <div class="row">
+                                <div class="col-md-5">Password:
+                        </div>
+                                <div class="col-md-5">
+                                    <input type="password" id="Password" placeholder="12345"></input>
+                                </div>
+                            </div><br></br>
+                            <div class="row">
+                                <div class="col-md-5">Contact Number:
+                        </div>
+                                <div class="col-md-5">
+                                    <input type="text" id="ContactNumber" placeholder="071404040" ></input>
+                                </div>
+                            </div><br></br>
+                            <div class="row">
+                            <div class="col-md-5"> <MDBBtn type="submit"  >Update</MDBBtn></div>
+                            <div class="col-md-5"> <MDBBtn type="submit" onClick={this.handleCancel}>Cancel</MDBBtn></div>
+                            </div>
+
+                      
+                        </form>
+
+                    </MDBCardBody>
+                </MDBCard>
+            </MDBCol>
+        );
+    }
+}
+
+export default Userprofilecard;
