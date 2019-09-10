42880de82c14e9ec494e054cf2293e271a2fe782
Ravinga Perera <ravinga_p@3logixs.lk>
2019-09-10 18:09:23 +0530

    convert date

--- src/Component/User/Cancelledbooking.js
+++ src/Component/User/Cancelledbooking.js
@@ -2,6 +2,21 @@ import React from 'react';
 import { MDBContainer, MDBCollapse, MDBCard, MDBCardBody, MDBBtn } from "mdbreact";
 
 export default class CancelledBooking extends React.Component{
+  constructor(props){
+    super(props);
+   
+    this.convertTime=this.convertTime.bind(this)
+    this.convertDate=this.convertDate.bind(this)
+  }
+
+  //converting date and time
+  convertTime(time) {
+    return (<p>{new Date(time).toUTCString().split(" ")[4]}</p>)
+  }
+
+  convertDate(OrderDate) {
+    return (<p>{new Date(OrderDate).toUTCString().split(" ").slice(1, 4).join(" ")}</p>)
+  }
     state={
         collapseID: "collapse3"
       }
@@ -26,29 +41,29 @@ export default class CancelledBooking extends React.Component{
                   <form style={{ textAlign: "left",display:"block" ,fontSize:"13px"}} name="showProfile">                              
                               <div class="row">
                                   <div class="col-md-5">Order Id: </div>
-                                  <div class="col-md-5">
-                                      <input type="text" name="OrderId" placeholder={this.props.OrderId} disabled></input>
+                                  <div class="col-md-5">{this.props.OrderId}
+                                      {/* <input type="text" name="OrderId" placeholder={this.props.OrderId} disabled></input> */}
                               </div>
                               </div><br></br> 
                                 {/*  */}
                               <div class="row">
                                   <div class="col-md-5">Worker Id: </div>
-                                  <div class="col-md-5">
-                                      <input type="text" name="WorkerId" placeholder={this.props.WorkerId} disabled></input>
+                                  <div class="col-md-5">{this.props.WorkerId}
+                                      {/* <input type="text" name="WorkerId" placeholder={this.props.WorkerId} disabled></input> */}
                               </div>
                               </div><br></br> 
                                 {/*  */}
                                 <div class="row">
                                   <div class="col-md-5">OrderDate: </div>
-                                  <div class="col-md-5">
-                                      <input type="text" name="OrderDate" placeholder={this.props.OrderDate} disabled></input>
+                                  <div class="col-md-5">{this.convertDate(this.props.OrderDate)}
+                                      {/* <input type="text" name="OrderDate" placeholder={this.props.OrderDate} disabled></input> */}
                               </div>
                               </div><br></br>
                                {/*  */}
                                <div class="row">
                                   <div class="col-md-5"> Cancellation Reason: </div>
-                                  <div class="col-md-5">
-                                      <input type="text" name="CancellationReason" placeholder={this.props.CancellationReason} disabled></input>
+                                  <div class="col-md-5">{this.props.CancellationReason}
+                                      {/* <input type="text" name="CancellationReason" placeholder={this.props.CancellationReason} disabled></input> */}
                               </div>
                               </div><br></br> 
                                
