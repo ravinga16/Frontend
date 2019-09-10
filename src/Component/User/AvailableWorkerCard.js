cd87ab4ad56719a4e8e23d7e48d4e8d6db977466
ravinga <37566894+ravinga16@users.noreply.github.com>
2019-09-10 20:56:11 +0530

    Update AvailableWorkerCard.js
    Capitalize all the words

--- src/Component/User/AvailableWorkerCard.js
+++ src/Component/User/AvailableWorkerCard.js
@@ -8,13 +8,13 @@ class AvailableWorkerCard extends React.Component{
                 <MDBCardBody>
                 <MDBCardTitle>{this.props.firstName} {this.props.lastName}</MDBCardTitle>
                 <MDBCardText>   
-                workerId:{this.props.workerId} <br></br>
-                firstName:{this.props.firstName} <br></br>
-                lastName:{this.props.lastName}<br></br>
-                baseLocation:{this.props.baseLocation}<br></br>
-                rate:{this.props.rate}<br></br>
-                skillDescription:{this.props.skillDescription}<br></br>
-                hourlyCharge:{this.props.hourlyCharge}<br></br>
+                WorkerId:{this.props.workerId} <br></br>
+                FirstName:{this.props.firstName} <br></br>
+                LastName:{this.props.lastName}<br></br>
+                BaseLocation:{this.props.baseLocation}<br></br>
+                Rate:{this.props.rate}<br></br>
+                SkillDescription:{this.props.skillDescription}<br></br>
+                HourlyCharge:{this.props.hourlyCharge}<br></br>
                 </MDBCardText>                
                 </MDBCardBody>
             </MDBCard>
